7b49114a699df18ccf5c6eb2d9da2f204fc3c8d0
ssoraka <t89043139690@mail.ru>
2020-08-10 20:12:55 +0300

    создал проект

--- main.c
+++ main.c
@@ -1,105 +1,43 @@
+//
+// Created by ssoraka on 10.08.2020.
+//
 #include "game3.h"
 
 /*
- * поиск точки минимума на карте в многомерном пространстве
- * функция должна представлять из себя что-то вроде
- * y = (x1 - b1)^2 + (x2 - b2)^2 + (x3 - b3)^2 + (x4 - b4)^2 + (x5 - b5)^2 + (x6 - b6)^2
+ * тема градиентный спуск
+ * глянь на досуге
+ * http://www.machinelearning.ru/wiki/index.php?title=%D0%9C%D0%B5%D1%82%D0%BE%D0%B4_%D0%B3%D1%80%D0%B0%D0%B4%D0%B8%D0%B5%D0%BD%D1%82%D0%BD%D0%BE%D0%B3%D0%BE_%D1%81%D0%BF%D1%83%D1%81%D0%BA%D0%B0
+ * нас интересует первая формула
+ *
+ * и поищи более понятные объяснения
+ *
+ * суть в том, что
+ * X{x0(i + 1), x1(i + 1),...} = X{x0(i) - a * df(x0)/dx0, x1(i) - a * df(x1)/dx1} =
+ * X{
+ * 		x0(i) - a * (f(x0(i), x1(i)) - f(x0(i - 1)), x1(i))/(x0(i) - x0(i - 1)),
+ * 		x1(i) - a * (f(x0(i), x1(i)) - f(x0(i), x1(i - 1)))/(x1(i) - x1(i - 1)),...
+ * }, где а - маленький размер шага
+ *
  */
-
-
-#define ALPHA 0.000005
-
-void init(double *x, int dim) {
-    int i = 0;
-    while (i < dim) {
-        x[i] = rand();
-        i++;
-    }
-}
-
-void init_prev(double *x, int dim) {
-    int i = 0;
-    while (i < dim) {
-        x[i] += 100;
-        i++;
-    }
-}
-
-void    get_dx(void *game, double y, double *dx, double *x, double *x_prev, int dim) {
-    int i;
-    double tmp;
-    double delta;
-
-    i = 0;
-    while (i < dim) {
-        tmp = x[i];
-        x[i] = x_prev[i];
-        delta = (y - function(game, x)) / (tmp - x_prev[i]);
-        if (delta == delta)
-            dx[i] = delta;
-        x[i] = tmp;
-        i++;
-    }
-}
-
-void    copy(double *x_prev, double *x, int dim) {
-    int i;
-
-    i = 0;
-    while (i < dim) {
-        x_prev[i] = x[i];
-        i++;
-    }
-}
-
-void    new_x(double *x, double *dx, int dim) {
-    int i;
-
-    i = 0;
-    while (i < dim) {
-        x[i] -= ALPHA * dx[i];
-        i++;
-    }
-}
-
-void    print(double *x, int dim) {
-    int i;
-
-    i = 0;
-    while (i < dim) {
-        printf("%lf_", x[i]);
-        i++;
-    }
-    printf("\n");
-}
 
 int main() {
 
-    double x[10];
-    double x_prev[10];
+	//инициализировал поле
+	void *game = init_game(0);
+	//получил количество измерений - 1 для нулевого уровня
+	int dim = get_game_dimension(game);
 
+	double x[1] = {rand()};
 
-    void *game = init_game(0);
-    int dim = get_game_dimension(game);
-    init(x, dim);
-    init_prev(x_prev, dim);
+	printf("мое случайное значение x=%lf, f(x)=%lf\n", x[0], function(game, x));
 
+	while (!is_this_answer(game, x)) {
 
-    double f_prev = function(game, x_prev);
-    double f = function(game, x);
+		x[0] = 0.0;
+		printf("в результате сложных поисков x=%lf, f(x)=%lf\n", x[0], function(game, x));
+	}
 
-    double dx[10];
-    while (!is_this_answer(game, x)) {
-        get_dx(game, f, dx, x, x_prev, dim);
+	destroy_game(&game);
 
-        f_prev = f;
-        copy(x_prev, x, dim);
-        new_x(x, dx, dim);
-        f = function(game, x);
-        print(x, dim);
-    }
-
-    destroy_game(&game);
-
-    return 0;
+	return 0;
 }
